3a2a7997292d2a9682854c24caeed7bbc3e06996
Zett0x <zetto1514@gmail.com>
2022-01-21 07:44:41 +0100

    permament auth state from redux implemented

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import { getAuth } from "firebase/auth";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
 } from "react-router-dom";
+import { login } from "../actions/auth";
 import { JournalScreen } from "../components/journal/JournalScreen";
+
 import { AuthRouter } from "./AuthRouter";
 
 export const AppRouter = () => {
+  const auth = getAuth();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user?.uid) {
+        dispatch(login(user.uid, user.displayName));
+      }
+    });
+  }, [dispatch]);
+
   return (
     <Router>
       <Routes>
