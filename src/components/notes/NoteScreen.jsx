0be5b72d40ba9d3c31ddc149dcb8dab5006e2795
Zett0x <zetto1514@gmail.com>
2022-02-20 21:07:24 +0100

    Importing startDeleting action from notes

--- src/components/notes/NoteScreen.jsx
+++ src/components/notes/NoteScreen.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { activeNote } from "../../actions/notes";
+import { activeNote, startDeleting } from "../../actions/notes";
 import { useForm } from "../../hooks/useForm";
 
 import { NotesAppBar } from "./NotesAppBar";
@@ -24,6 +24,11 @@ export const NoteScreen = () => {
   useEffect(() => {
     dispatch(activeNote(formValues.id, { ...formValues }));
   }, [formValues, dispatch]);
+
+  const handleDelete = () => {
+    //dispatch
+    dispatch(startDeleting(note.id));
+  };
 
   return (
     <div className="notes__main-content">
@@ -52,6 +57,9 @@ export const NoteScreen = () => {
           </div>
         )}
       </div>
+      <button className="btn btn-danger" onClick={handleDelete}>
+        Delete
+      </button>
     </div>
   );
 };
